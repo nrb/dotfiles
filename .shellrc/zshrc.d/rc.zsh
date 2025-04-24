d4a268ba93c57bf5e1398e2cca5201bd031908bf
Nolan Brubaker <nolan@nbrubaker.com>
2025-04-24 16:48:03 -0400

    Update fzf shell keybinds to the modern method
    Signed-off-by: Nolan Brubaker <nolan@nbrubaker.com>

--- .shellrc/zshrc.d/rc.zsh
+++ .shellrc/zshrc.d/rc.zsh
@@ -197,8 +197,6 @@ export GOENV_ROOT="$HOME/.goenv"
 export PATH="$GOENV_ROOT/bin:$PATH"
 eval "$(goenv init -)"
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
@@ -212,3 +210,5 @@ source /Users/nbrubake/.config/op/plugins.sh
 
 # pipx installed scripts
 export PATH="$PATH:/Users/nbrubake/.local/bin"
+
+source <(fzf --zsh)
